1da0bae47dec9e1bf530543377991e0691daa31f
Thomas Robitaille <thomas.robitaille@gmail.com>
2010-11-19 15:57:38 -0500

    Major improvements, and added start of README

--- mysg/models.py
+++ mysg/models.py
@@ -3,10 +3,8 @@ import random
 import numpy as np
 import atpy
 
-from mysg.odict import odict
-from mysg.ranges import read_ranges, write_ranges
+from mysg.ranges import read_ranges, write_ranges, select_required_ranges
 from mysg.util import create_dir, random_id
-from mysg.parameters import required_parameters
 
 
 def sample_set_models(set_name, number, seed=123456789):
@@ -18,7 +16,7 @@ def sample_set_models(set_name, number, seed=123456789):
     # Read in the ranges
     ranges = read_ranges("models/%s/ranges.conf" % set_name)
 
-    # First pass, sample all values that need to be sampled
+    # Sample all values that need to be sampled
     values = atpy.Table()
     for name in ranges:
         par = ranges[name]
@@ -31,17 +29,12 @@ def sample_set_models(set_name, number, seed=123456789):
                               10. ** np.random.uniform(np.log10(par['lower']),
                                                        np.log10(par['upper']),
                                                        number))
-        elif par['sampling'] == 'fixed':
+        elif par['sampling'] in ['fixed', 'str']:
             values.add_column(name, np.repeat(par['value'], number))
         elif par['sampling'] == 'linked':
-            pass
+            values.add_column(name, values[ranges[name]['parameter']])
         else:
             raise Exception("Unknown sampling: %s" % par['sampling'])
-
-    # Second pass, deal with linked parameters
-    for name in ranges:
-        if ranges[name]['sampling'] == 'linked':
-            values.add_column(name, values[ranges[name]['parameter']])
 
     # Write out parameter files
     create_dir("models/%s/par" % set_name)
@@ -50,7 +43,10 @@ def sample_set_models(set_name, number, seed=123456789):
         model_name = random_id()
         f = open("models/%s/par/%s.par" % (set_name, model_name), 'wb')
         for name in values.columns:
-            f.write("%s = %9.3e\n" % (name, values[name][i]))
+            if type(values[name][i]) not in [str, np.string_]:
+                f.write("%s = %9.3e\n" % (name, values[name][i]))
+            else:
+                f.write("%s = %s\n" % (name, values[name][i]))
         f.close()
 
     # Write out table
@@ -63,23 +59,14 @@ def make_set_dir(set_name, ranges_file):
     directory with a subset of the ranges file
     '''
 
-    # Find out which parameters are required
-    required = required_parameters(set_name)
-
     # Read in ranges
     ranges = read_ranges(ranges_file)
 
-    # Define new subset of ranges
-    ranges_new = odict()
-    for parameter in required:
-        if ranges[parameter]['sampling'] == 'linked' \
-           and ranges[parameter]['parameter'] not in required:
-            ranges_new[parameter] = ranges[ranges[parameter]['parameter']]
-        else:
-            ranges_new[parameter] = ranges[parameter]
+    # Select ranges that are actually needed
+    ranges = select_required_ranges(set_name, ranges)
 
     # Create directory
     create_dir('models/%s' % set_name)
 
     # Write out ranges file
-    write_ranges('models/%s/ranges.conf' % set_name, ranges_new)
+    write_ranges('models/%s/ranges.conf' % set_name, ranges)
--- mysg/ranges.py
+++ mysg/ranges.py
@@ -7,13 +7,16 @@ def write_ranges(filename, ranges):
     for name in ranges:
         par = ranges[name]
         if par['sampling'] in ['log10', 'linear']:
-            f.write("%-20s %-10s %9.3e  %9.3e\n"
+            f.write("%-25s %-10s %10.3e  %10.3e\n"
                     % (name, par['sampling'], par['lower'], par['upper']))
         elif par['sampling'] in ['fixed']:
-            f.write("%-20s %-10s %9.3e\n"
+            f.write("%-25s %-10s %10.3e\n"
+                    % (name, par['sampling'], par['value']))
+        elif par['sampling'] in ['str']:
+            f.write("%-25s %-10s  %s\n"
                     % (name, par['sampling'], par['value']))
         elif par['sampling'] in ['linked']:
-            f.write("%-20s %-10s %s\n"
+            f.write("%-25s %-10s  %s\n"
                     % (name, par['sampling'], par['parameter']))
         else:
             raise Exception("Unknown sampling: %s" % par['sampling'])
@@ -32,14 +35,66 @@ def read_ranges(filename):
             lower, upper = details.split()
             ranges[name]['lower'] = float(lower)
             ranges[name]['upper'] = float(upper)
-        if sampling in ['fixed']:
+        elif sampling in ['fixed']:
             ranges[name]['value'] = float(details)
-        elif sampling == 'linked':
+        elif sampling in ['str']:
+            ranges[name]['value'] = details
+        elif sampling in ['linked']:
             ranges[name]['parameter'] = details
         else:
             raise Exception("Unknown sampling: %s" % sampling)
     f.close()
     return ranges
+
+
+def select_required_ranges(set_name, ranges):
+
+    # Add parameters
+    required = []
+    if set_name[0] == 's':
+        required += ['star.radius', 'star.temperature']
+    if set_name[1] == 'd':
+        required += ['disk.rmax', 'disk.mass', 'disk.beta', 'disk.h100']
+    if set_name[2] == 'u':
+        required += ['envelope.rc', 'envelope.rho0']
+    if set_name[2] == 'p':
+        required += ['envelope.rmin', 'envelope.power', 'envelope.mass']
+    if set_name[3] == 'c':
+        required += ['cavity.theta0', 'cavity.rho0', 'cavity.rho_exp', 'cavity.power']
+    if set_name[4] == 'm':
+        required += ['ambient.density', 'ambient.temperature']
+    if set_name[5] == 'h':
+        if set_name[1] == 'd':
+            required += ['disk.rmin']
+        if set_name[1] == 'e':
+            required += ['envelope.rmin']
+    if set_name[6] == 'a':
+        required += ['disk.lacc']
+    if set_name[7] == 'g'
+        required += ['disk.eta']
+
+    for parameter in required:
+        if parameter not in ranges:
+            raise Exception("Parameter %s missing from ranges" % parameter)
+
+    ranges_new = odict()
+    for parameter in required:
+        if ranges[parameter]['sampling'] == 'linked' \
+           and ranges[parameter]['parameter'] not in required:
+            ranges_new[parameter] = ranges[ranges[parameter]['parameter']]
+        else:
+            ranges_new[parameter] = ranges[parameter]
+
+    # Sets parameters that depend on flags
+    if set_name[8] == 'g':
+        ranges['disk.dust.amax'] = 1000.
+    if set_name[9] == 'p':
+        ranges['disk.dust.pah_frac'] = 0.18
+        ranges['envelope.dust.pah_frac'] = 0.18
+        ranges['cavity.dust.pah_frac'] = 0.18
+        ranges['ambient.dust.pah_frac'] = 0.18
+
+    return ranges_new
 
 # In future, can define function to find intersection of two ranges or
 # compare the parameter space volume of two set of ranges.
