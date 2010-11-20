437ae51b0a38c753416b138b5e65685089893dfd
Thomas Robitaille <thomas.robitaille@gmail.com>
2010-11-20 14:20:55 -0500

    New routines for writing out parameter files

---
+++ mysg/parameters.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from mysg.odict import odict
+
+
+def write_parfile(filename, parameters):
+    f = open(filename, 'wb')
+    for name in parameters:
+        if type(parameters[name]) not in [str, np.string_]:
+            f.write("%s = %9.3e\n" % (name, parameters[name]))
+        else:
+            f.write("%s = %s\n" % (name, parameters[name]))
+    f.close()
+    
+    
+def read_parfile(filename, nested=False):
+    parameters = odict()
+    f = open(filename, 'rb')
+    for line in f.readlines():
+        print line
+        key, value = line.strip().split('=')
+        if nested:
+            p = parameters
+            components = key.split('.')
+            for component in components[:-1]:
+                if component not in p:
+                    p[component] = odict()
+                p = p[component]
+            p[components[-1]] = value
+        else:
+            parameters[key] = value
+    f.close()
+    return parameters
+    
