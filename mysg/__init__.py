4ebcd037bfd6ef43081598961545a9dd82141855
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-07-25 15:57:00 -0400

    Updated version string to 0.1.1

--- mysg/__init__.py
+++ mysg/__init__.py
@@ -2,3 +2,5 @@ import models
 import parameters
 import ranges
 import atmosphere
+
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:  # Python 2.x
     from distutils.command.build_py import build_py
 
 setup(name='Model YSO SED Grid',
-      version='0.1.0',
+      version='0.1.1',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
       packages=['mysg'],
